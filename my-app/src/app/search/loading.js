6fc08eca94af4310c9676ad72bb6480ce17a022d
john-stouffer <stouff17@msu.edu>
2025-05-20 21:35:54 -0500

    will this be the correct github?

--- my-app/src/app/search/loading.js
+++ my-app/src/app/search/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="flex w-lvw h-lvh justify-center content-center">
-            <h1>Loading...</h1>
+            <h1>Loading... with github issues bro</h1>
         </div>
     );
 }
